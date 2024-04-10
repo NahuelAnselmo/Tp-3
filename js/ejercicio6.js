11ac0451ca71e0d21e1e47f35ea3bb45eba4789b
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2024-04-10 19:52:38 -0300

    Ejercicio 6 terminado papaaaa

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -3,6 +3,17 @@ valor de los lados de un rectángulo, luego crear una
 función para calcular su perímetro y mostrarlo por 
 pantalla. */
 
-function calcularPerimetro(perimetro, lado1, lado2) {
+function calcularPerimetro(lado1, lado2) {
     
+    let perimetro = 0;
+    perimetro = 2 * (lado1 + lado2);
+
+    return perimetro;
 }
+
+let lado1 = parseFloat(prompt("ingresa el primer valor del rectangulo"));
+let lado2 = parseFloat(prompt("ingresa el segundo valor del rectangulo"));
+
+resultado = calcularPerimetro(lado1, lado2);
+
+document.write("El perimetro del rectangulo es: ", resultado);
